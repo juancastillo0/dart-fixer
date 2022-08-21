9a142abbe7e646e1a1a240a90efea8ce4ce93e58
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2022-08-21 02:51:31 +0000

    Dart Function test

--- src/test/parser.test.ts
+++ src/test/parser.test.ts
@@ -60,6 +60,260 @@ class Name4< WP  extends String?>{
 
     const c4 = classesObjects[3];
     assert.deepStrictEqual(c4.name, "Name4");
-    assert.deepStrictEqual(c4.extendsBound, undefined);
+    assert.deepStrictEqual(c4.extendsBound, null);
+  });
+
+  test("Dart Function", () => {
+    const text = `
+final str = '''
+String? func2() {
+}
+''';
+
+class B extends Other {
+  final int? v;
+  final String value;
+
+  B({
+    this.v = 3,
+    super.md = const [],
+    required this.value,
+  });
+
+  bool get isVNull => v == null;
+
+  int valueLength({bool? sum}) => value.length;
+  Map<String , List<int? >> valueLengthReq(
+    Object v, { required bool? sum}) =>{}
+    ;
+
+  set isVNull(bool newValue) {}
+
+  static compare<N extends num>(B self, [List<N>  def = const [2 ,]]) => self.isVNull;
+
+  static int? compareInt([B? self]) => self?.v;
+}
+
+final str2 = '''
+  String? func2() {
+  }
+''';
+
+class G<T extends B?> {}
+
+String func(G<B?> d) {
+  return jsonDecode('{}');
+}   
+`;
+
+    const values = new DartImports(text);
+    console.log(values);
+    // console.log(JSON.stringify(values, null, 2));
+
+    const classes = values.classes;
+    const bClass = classes[0];
+    assert.strictEqual(bClass.name, "B");
+    assert.strictEqual(bClass.extendsBound, "Other");
+    assert.strictEqual(bClass.isAbstract, false);
+
+    assert.deepStrictEqual(removeMatch(bClass.fields), [
+      {
+        isStatic: false,
+        isFinal: true,
+        name: "v",
+        isVariable: false,
+        type: "int?",
+        defaultValue: null,
+        dartClass: bClass,
+      },
+      {
+        isStatic: false,
+        isFinal: true,
+        name: "value",
+        isVariable: false,
+        type: "String",
+        defaultValue: null,
+        dartClass: bClass,
+      },
+    ]);
+
+    const methods = [
+      // bool get isVNull => v == null;
+      {
+        isStatic: false,
+        returnType: "bool",
+        isGetter: true,
+        isSetter: false,
+        name: "isVNull",
+        generics: null,
+        params: [],
+        dartClass: bClass,
+      },
+      // int valueLength({bool? sum}) => value.length;
+      {
+        isStatic: false,
+        returnType: "int",
+        isGetter: false,
+        isSetter: false,
+        name: "valueLength",
+        generics: null,
+        params: [
+          {
+            isRequired: false,
+            isNamed: true,
+            defaultValue: null,
+            name: "sum",
+            type: "bool?",
+            dartFunction: values.functions[1],
+          },
+        ],
+        dartClass: bClass,
+      },
+      // Map<String , List<int >> valueLengthReq(
+      //   Object v, { required bool? sum}) =>{}
+      //   ;
+      {
+        isStatic: false,
+        returnType: "Map<String , List<int? >>",
+        isGetter: false,
+        isSetter: false,
+        name: "valueLengthReq",
+        generics: null,
+        params: [
+          // Object v, { required bool? sum}
+          {
+            isRequired: true,
+            isNamed: false,
+            defaultValue: null,
+            name: "v",
+            type: "Object",
+            dartFunction: values.functions[2],
+          },
+          {
+            isRequired: true,
+            isNamed: true,
+            defaultValue: null,
+            name: "sum",
+            type: "bool?",
+            dartFunction: values.functions[2],
+          },
+        ],
+        dartClass: bClass,
+      },
+
+      // set isVNull(bool newValue) {}
+      {
+        isStatic: false,
+        returnType: null,
+        isGetter: false,
+        isSetter: true,
+        name: "isVNull",
+        generics: null,
+        params: [
+          {
+            isRequired: true,
+            isNamed: false,
+            defaultValue: null,
+            name: "newValue",
+            type: "bool",
+            dartFunction: values.functions[3],
+          },
+        ],
+        dartClass: bClass,
+      },
+      // static compare<N extends num>(B self, [List<N>  def = const [2 ,]]) => self.isVNull;
+      {
+        isStatic: true,
+        isGetter: false,
+        isSetter: false,
+        returnType: null,
+        name: "compare",
+        generics: "<N extends num>",
+        params: [
+          {
+            isRequired: true,
+            isNamed: false,
+            defaultValue: null,
+            name: "self",
+            type: "B",
+            dartFunction: values.functions[4],
+          },
+          {
+            isRequired: false,
+            isNamed: false,
+            defaultValue: "const [2 ,]",
+            name: "def",
+            type: "List<N>",
+            dartFunction: values.functions[4],
+          },
+        ],
+        dartClass: bClass,
+      },
+      // static int? compareInt([B? self]) => self?.v;
+      {
+        isStatic: true,
+        isGetter: false,
+        isSetter: false,
+        returnType: "int?",
+        name: "compareInt",
+        generics: null,
+        params: [
+          {
+            isRequired: false,
+            isNamed: false,
+            defaultValue: null,
+            name: "self",
+            type: "B?",
+            dartFunction: values.functions[5],
+          },
+        ],
+        dartClass: bClass,
+      },
+    ];
+
+    assert.deepStrictEqual(removeMatch(bClass.methods), methods);
+
+    assert.deepStrictEqual(removeMatch(values.functions), [
+      ...methods,
+      // String func(G<B?> d) {
+      //   return jsonDecode('{}');
+      // }
+      {
+        dartClass: null,
+        isStatic: false,
+        isGetter: false,
+        isSetter: false,
+        returnType: "String",
+        name: "func",
+        generics: null,
+        params: [
+          {
+            isRequired: true,
+            isNamed: false,
+            defaultValue: null,
+            name: "d",
+            type: "G<B?>",
+            dartFunction: values.functions[6],
+          },
+        ],
+      },
+    ]);
   });
 });
+
+function removeMatch(values: Array<unknown>): Array<unknown> {
+  return values.map((v) => {
+    if (typeof v !== "object") {
+      return v;
+    }
+    const obj: Record<string, unknown> = { ...v };
+    // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+    delete obj["match"];
+    for (const [key, value] of [...Object.entries(obj)]) {
+      if (Array.isArray(value)) {
+        obj[key] = removeMatch(value);
+      }
+    }
+    return obj;
+  });
+}
