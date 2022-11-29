4b9ff5b32e60ab98e4aa604b7f3fa000a20509fa
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2022-11-29 04:37:50 +0000

    add dart test workspace

---
+++ test_workspace/bin/test_workspace.dart
@@ -0,0 +1,5 @@
+import 'package:test_workspace/test_workspace.dart' as test_workspace;
+
+void main(List<String> arguments) {
+  print('Hello world: ${test_workspace.calculate()}!');
+}
---
+++ test_workspace/test/test_workspace_test.dart
@@ -0,0 +1,8 @@
+import 'package:test_workspace/test_workspace.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
