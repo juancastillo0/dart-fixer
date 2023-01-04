0f36e1e2dc83ce898ee7ad02f6eb162f9c2e4476
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2023-01-04 20:54:24 +0000

    add timeout for vscode extension integration tests

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -7,6 +7,7 @@ export function run(): Promise<void> {
   const mocha = new Mocha({
     ui: "tdd",
     color: true,
+    timeout: 7000,
   });
 
   const testsRoot = path.resolve(__dirname, "..");
@@ -21,6 +22,7 @@ export function run(): Promise<void> {
       files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
 
       try {
+        // TODO: coverage for vscode extension integration tests
         // Run the mocha test
         mocha.run((failures) => {
           if (failures > 0) {
