898b4f6e0e097130b9b72c65b27f828e037d2491
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2023-01-15 02:40:10 +0000

    improve markdown table, support example,description,default and tagged unions

--- src/json-type-definition/markdown-table.ts
+++ src/json-type-definition/markdown-table.ts
@@ -10,6 +10,20 @@ import {
   SomeJTDSchemaTypeUnion,
 } from "./schema-type";
 
+interface ObjectTableOptions {
+  props: Array<[string, SomeJTDSchemaType]>;
+  requiredProps: Array<string>;
+  hasDescription: boolean;
+  hasDefault: boolean;
+  hasExample: boolean;
+}
+
+export enum JTDToTableSchemaKey {
+  description = "description",
+  default = "default",
+  examples = "examples",
+}
+
 export class JTDToTable {
   current = "";
 
@@ -17,75 +31,129 @@ export class JTDToTable {
 
   mapJTDToTable = (
     name: string,
-    schema: SomeJTDSchemaTypeObject
+    schema: SomeJTDSchemaTypeObject | SomeJTDSchemaTypeUnion
   ): Array<{ name: string; table: string }> => {
     const prev = this.current;
+    // TODO: use title metadata
     this.current = name;
-    // TODO: definitions
+
+    const isUnion = "mapping" in schema;
+    let options: ObjectTableOptions;
+    if (isUnion) {
+      const optionsList = Object.values(schema.mapping).map(
+        this.getObjectTableOptions
+      );
+      options = optionsList.shift()!;
+      optionsList.forEach((c) => {
+        options.hasDefault = options.hasDefault || c.hasDefault;
+        options.hasDescription = options.hasDescription || c.hasDescription;
+        options.hasExample = options.hasExample || c.hasExample;
+      });
+    } else {
+      options = this.getObjectTableOptions(schema);
+    }
+    const columns = [
+      isUnion ? `|Union Tag (${schema.discriminator})` : undefined,
+      "Name",
+      "Type",
+      "Required",
+      options.hasDescription ? "Description" : undefined,
+      options.hasDefault ? "Default" : undefined,
+      options.hasExample ? "Example" : undefined,
+    ].filter((c) => !!c);
+
+    let rows: Array<string>;
+    if (isUnion) {
+      rows = Object.entries(schema.mapping).flatMap(([k, v]) =>
+        this.getObjectPropsRows({
+          ...this.getObjectTableOptions(v),
+          hasDefault: options.hasDefault,
+          hasDescription: options.hasDescription,
+          hasExample: options.hasExample,
+        }).map((row, i) => `|${i === 0 ? k : ""}${row}`)
+      );
+    } else {
+      rows = this.getObjectPropsRows(options);
+    }
+
+    const mainTable =
+      `|${columns.join("|")}|\n|${columns.map(() => " --- ").join("|")}|\n` +
+      rows.join("\n");
+    const tables: Array<{ name: string; table: string }> = [];
+    tables.push({ name: this.current, table: mainTable });
+    tables.push(
+      ...[...options.props, ...Object.entries(schema.definitions ?? {})]
+        .filter(
+          ([, type]) =>
+            "properties" in type ||
+            "optionalProperties" in type ||
+            "mapping" in type
+        )
+        .flatMap(([propName, type]) =>
+          this.mapJTDToTable(
+            propName,
+            type as SomeJTDSchemaTypeObject | SomeJTDSchemaTypeUnion
+          )
+        )
+    );
+    this.current = prev;
+    return tables;
+  };
+
+  private getObjectTableOptions = (
+    schema: SomeJTDSchemaTypeObject
+  ): ObjectTableOptions => {
     const props = [
       ...Object.entries(schema.properties ?? {}),
       ...Object.entries(schema.optionalProperties ?? {}),
     ];
     const requiredProps = Object.keys(schema.properties ?? {});
     const hasDescription = props.some(
-      ([, v]) => typeof v.metadata?.["description"] === "string"
+      ([, v]) =>
+        typeof v.metadata?.[JTDToTableSchemaKey.description] === "string"
     );
     const hasDefault = props.some(
       ([, v]) =>
-        typeof v.metadata?.["default"] !== "undefined" &&
-        v.metadata?.["default"] !== null
+        typeof v.metadata?.[JTDToTableSchemaKey.default] !== "undefined" &&
+        v.metadata?.[JTDToTableSchemaKey.default] !== null
     );
     const hasExample = props.some(
       ([, v]) =>
-        typeof v.metadata?.["example"] !== "undefined" &&
-        v.metadata?.["example"] !== null
+        typeof v.metadata?.[JTDToTableSchemaKey.examples] !== "undefined" &&
+        v.metadata?.[JTDToTableSchemaKey.examples] !== null
     );
-    const columns = [
-      "Name",
-      "Type",
-      "Required",
-      hasDescription ? "Description" : undefined,
-      hasDefault ? "Default" : undefined,
-      hasExample ? "Example" : undefined,
-    ].filter((c) => !!c);
 
-    const mainTable =
-      `|${columns.join("|")}|\n|${columns.map(() => " --- ").join("|")}|\n` +
-      props
-        .map(
-          ([propName, type]) =>
-            `|${propName}|${this.mapJTDToType(type, propName)}|${
-              requiredProps.includes(propName) && !type.nullable
-                ? "true"
-                : "false"
-            }|${
-              hasDescription
-                ? (type.metadata?.["description"] as string) ?? "" + "|"
-                : ""
-            }${
-              hasDefault
-                ? (type.metadata?.["default"] as string) ?? "" + "|"
-                : ""
-            }${
-              hasExample
-                ? (type.metadata?.["example"] as string) ?? "" + "|"
-                : ""
-            }`
-        )
-        .join("\n");
-    const tables: Array<{ name: string; table: string }> = [];
-    tables.push({ name: this.current, table: mainTable });
-    tables.push(
-      ...props
-        .filter(
-          ([, type]) => "properties" in type || "optionalProperties" in type
-        )
-        .flatMap(([propName, type]) =>
-          this.mapJTDToTable(propName, type as SomeJTDSchemaTypeObject)
-        )
-    );
-    this.current = prev;
-    return tables;
+    return {
+      props,
+      requiredProps,
+      hasDescription,
+      hasDefault,
+      hasExample,
+    };
+  };
+
+  private getObjectPropsRows = (opts: ObjectTableOptions): Array<string> => {
+    return opts.props.map(([propName, type]) => {
+      const m = type.metadata;
+      return `|${propName}|${this.mapJTDToType(type, propName)}|${
+        opts.requiredProps.includes(propName) && !type.nullable
+          ? "true"
+          : "false"
+      }|${
+        opts.hasDescription
+          ? jsonStringToMarkdown(m?.[JTDToTableSchemaKey.description] ?? "") +
+            "|"
+          : ""
+      }${
+        opts.hasDefault
+          ? jsonStringToMarkdown(m?.[JTDToTableSchemaKey.default] ?? "") + "|"
+          : ""
+      }${
+        opts.hasExample
+          ? jsonStringToMarkdown(m?.[JTDToTableSchemaKey.examples] ?? "") + "|"
+          : ""
+      }`;
+    });
   };
 
   mapJTDToType = (schema: SomeJTDSchemaType, name?: string): string => {
@@ -96,17 +164,17 @@ export class JTDToTable {
     const value = mapJTD(schema, {
       ref: (s) => `${s.ref}`,
       primitive: (s) => `${s.type}`,
-      enum: (s) => `["${s.enum.join('","')}"]`,
+      enum: (s) => `"${s.enum.join('" \\| "')}"`,
       array: (s) =>
-        `List<${this.mapJTDToType(s.elements, `${this.current}Element`)}>`,
+        `Array<${this.mapJTDToType(s.elements, `${this.current}Element`)}>`,
       map: (s) =>
-        `Map<String,${this.mapJTDToType(s.values, `${this.current}Value`)}>`,
+        `Map<string,${this.mapJTDToType(s.values, `${this.current}Value`)}>`,
       object: () => `${this.current}`,
       union: (s) =>
         `${Object.entries(s.mapping)
           .map(([key, type]) => this.mapJTDToType(type, key))
-          .join(" | ")}`,
-      base: (_) => `dynamic`,
+          .join(" \\| ")}`,
+      base: (_) => `any`,
     });
     this.current = prev;
 
@@ -116,6 +184,29 @@ export class JTDToTable {
     return value;
   };
 }
+
+const jsonStringToMarkdown = (value: unknown): string => {
+  if (Array.isArray(value)) {
+    return (
+      "<ul><li>" +
+      value.map(jsonStringToMarkdown).join("</li><li>") +
+      "</li></ul>"
+    );
+  } else if (value === undefined || value === null) {
+    return "null";
+  }
+
+  let str: string;
+  // eslint-disable-next-line @typescript-eslint/no-base-to-string
+  str = (value as object).toString();
+  if (str === "[object Object]") {
+    str = JSON.stringify(value);
+  }
+  return str
+    .replace(/\n\n/g, "<br/>")
+    .replace(/\n/g, " ")
+    .replace(/(?=[^\\])\|/g, "\\|");
+};
 
 const mapJTD = <T>(
   schema: SomeJTDSchemaType,
