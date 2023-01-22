adb163253f4dff715fbced5546496152069ec8ec
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2023-01-22 19:58:26 +0000

    more GenerationOptions docs and baseClass config object

--- src/generator/generator-config.ts
+++ src/generator/generator-config.ts
@@ -1,6 +1,126 @@
 import { AjvJTDSchemaType, compileCustomValidator } from "../utils";
 
+/** The main configuration options to generate Dart models classes.
+ *
+ * @example
+ * A serializable model using `package:equatable` for equality comparison and a builder class:
+ *
+ * ```json
+ * {
+ *  "baseClass": {
+ *    "mixins": ["Serializable"],
+ *    "extends": "Equatable",
+ *    "customImports": ["import '/models/serde.dart';","import 'package:equatable/equatable.dart' show Equatable;"]
+ *  },
+ *  "fromJson":{
+ *    "parameterType":"Object?"
+ *  },
+ *  "toJson":{
+ *    "metadata":"/// Returns a Map of JSON values with all the fields.\n/// The `fromJson` factory should recreate the instance.\n@override"
+ *  },
+ *  "toStringOverride":{},
+ *  "builder":{},
+ *  "allFieldsGetter":{
+ *    "metadata":"@override"
+ *   }
+ * }
+ * ```
+ *
+ * This configuration will generate a Dart class of the following structure (assuming the model
+ * has a property "prop" of integer type):
+ *
+ * ```dart
+ * import '/models/serde.dart';
+ * import 'package:equatable/equatable.dart' show Equatable;
+ *
+ * class Name extends Equatable with Serializable {
+ *    final int prop;
+ *
+ *    const Name({required this.prop});
+ *
+ *    factory Name.fromJson(Object? json) {
+ *      final _json = json as Map;
+ *      return Name(prop: _json['prop'] as int);
+ *    }
+ *
+ *    /// Returns a Map of JSON values with all the fields.
+ *    /// The `fromJson` factory should recreate the instance.
+ *    @override
+ *    Map<String, Object?> toJson() {
+ *      return {'prop': prop};
+ *    }
+ *
+ *    @override
+ *    String toString() {
+ *      return `Name${{'prop': prop}}`
+ *    }
+ *
+ *    @override
+ *    List<Object?> get props => [prop];
+ * }
+ *
+ * class NameBuilder {
+ *  bool propIsSet = false;
+ *  int? _prop;
+ *  int? get prop => _prop;
+ *  set prop(int? newValue) {
+ *    _prop = newValue;
+ *    propIsSet = true;
+ *  }
+ *  NameBuilder propSet(int newValue) {
+ *    prop = newValue;
+ *    return this;
+ *  }
+ *
+ *  bool get isValidValue {
+ *    return prop != null;
+ *  }
+ *  Name? tryToValue() {
+ *   if (!isValidValue) {
+ *     return null;
+ *   }
+ *    return Name(prop: prop as int);
+ *  }
+ * }
+ * ```
+ *
+ * @example A simple data class
+ *
+ * ```json
+ * {
+ *  "equality":{
+ *    "deep":true
+ *  },
+ *  "copyWith":{
+ *    "metadata":"/// Returns a copy of the instance overriding the valued passed as arguments"
+ *  },
+ *  "toStringOverride":{
+ *    "metadata":"/// A String with the name of the class and all fields of the instance for debugging purposes"
+ *  }
+ * }
+ * ```
+ */
 export interface GenerationOptions {
+  /** General configuration for the generation of the model's Dart class */
+  baseClass?: {
+    /** The name of the model Dart class.
+     * @example "{{name}}Model" */
+    className?: string;
+    /** The types this class implements. This is the `implements` list in the type signature */
+    implements?: Array<string>;
+    /** The mixins this class has. This is the `with` list in the type signature */
+    mixins?: Array<string>;
+    /** The type this class extends. This is the `extends` type in the type signature
+     * @example "Equatable" */
+    extends?: string;
+    /** Additional imports for the class.
+     * @example ["import 'package:equatable/equatable.dart' show Equatable;"] */
+    customImports?: Array<string>;
+    /** Additional code inside the class definition. */
+    codeInClass?: Array<string>;
+    /** Additional code outside of the class definition. */
+    codeOutOfClass?: Array<string>;
+  };
   /** Generates a `Model.fromJson(Map json)` factory inside the class. */
   fromJson?: {
     /** The name of the constructor factory.
@@ -15,7 +135,8 @@ export interface GenerationOptions {
   /** Generates a `Map<String, Object?> toJson()` method that
    * returns a dart `Map<String, Object?>` with json values. */
   toJson?: {
-    /** Whether call the `toJson` method for nested values. */
+    /** Whether call the `toJson` method for nested values.
+     * @default false */
     nested?: boolean;
     /** You may put Dart comments or annotations for the method */
     metadata?: string;
@@ -47,7 +168,8 @@ export interface GenerationOptions {
   /** Generates a `String toString()` method override that returns a String
    * with all the field values. */
   toStringOverride?: {
-    /** You may put Dart comments or annotations for the method */
+    /** You may put Dart comments or annotations for the method.
+     * The @override annotation is always set. */
     metadata?: string;
   };
   /** Generates a `List<Object?> get props` getter that returns the values for all fields. */
@@ -107,9 +229,31 @@ export interface GenerationOptions {
 
 export const generationOptionsSchema: AjvJTDSchemaType<GenerationOptions> = {
   metadata: {
-    title: "GenerationOptions"
+    title: "GenerationOptions",
   },
   optionalProperties: {
+    // TODO: implement baseClass config
+    baseClass: {
+      optionalProperties: {
+        className: { type: "string" },
+        implements: {
+          elements: { type: "string" },
+        },
+        mixins: {
+          elements: { type: "string" },
+        },
+        extends: { type: "string" },
+        customImports: {
+          elements: { type: "string" },
+        },
+        codeInClass: {
+          elements: { type: "string" },
+        },
+        codeOutOfClass: {
+          elements: { type: "string" },
+        },
+      },
+    },
     toJson: {
       optionalProperties: {
         nested: { type: "boolean" },
